33b24bfc402baa4525a082882cac778a49c0d381
Bill Skrzypczak <bskrzypczak@mchenry.edu>
2017-09-24 10:34:01 -0500

    Rev 1

--- BillsAwesomeWeather/ViewController.swift
+++ BillsAwesomeWeather/ViewController.swift
@@ -12,14 +12,33 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if let url = NSURL(string: "https://api.darksky.net/forecast/ae58c5fa7285b492f6a553d200018d9e/42.5917,88.4334") {
+            if let data = NSData(contentsOf: url as URL){
+                do {
+                    let parsed = try JSONSerialization.jsonObject(with: data as Data, options: JSONSerialization.ReadingOptions.allowFragments) as! [String:AnyObject]
+                    
+                    let newDict = parsed 
+                    print(newDict["currently"]!["summary"])
+                    print(newDict["currently"]!["temperature"])
+                    print(newDict["currently"]!["dewPoint"])
+                    
+                    self.currentTemp.text = "\(newDict["currently"]!["temperature"]!!)"
+                    self.currentSummary.text = "\(newDict["currently"]!["summary"]!!)"
+
+                    
+                    
+                }
+                catch let error as NSError {
+                    print("A JSON parsithng error occurred, here are the details:\n \(error)")
+                }
+                
+            }
+        }
     }
+  
+    @IBOutlet weak var currentTemp: UILabel!
+    @IBOutlet weak var currentSummary: UILabel!
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
-}
+   }
 
